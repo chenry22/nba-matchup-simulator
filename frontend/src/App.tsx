924576feb091f74b1a22b0eb36908af53ac858a3
chenry22 <112424609+chenry22@users.noreply.github.com>
2026-07-27 15:44:55 -0400

    fix routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import AboutPage from "./pages/AboutPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/nba-matchup-simulator">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
